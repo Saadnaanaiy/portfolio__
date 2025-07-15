76ef8d22887bfa347ac713e6741ca532de592a47
saadnaanaiy <saadnaanaiy@gmail.com>
2025-07-15 20:53:25 +0100

    fixing all errors and add some upgrades!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+/** @type {import('tailwindcss').Config} */ export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,23 +11,10 @@ export default {
         darkHover: '#2a004a',
         darkThem: '#11001f',
       },
-
-      fontFamily: {
-        Outfit: ['Outfit', 'sans-serif'],
-        Ovo: ['Ovo', 'serif'],
-      },
-
-      boxShadow: {
-        black: '4px 4px 0 #000',
-        white: '4px 4px 0 #fff',
-      },
-      gridTemplateColumns: {
-        auto: 'repeat(auto-fit, minmax(200px, 1fr))',
-      },
-
-      animation: {
-        'fade-in': 'fadeIn 1s ease-out',
-      },
+      fontFamily: { Outfit: ['Outfit', 'sans-serif'], Ovo: ['Ovo', 'serif'] },
+      boxShadow: { black: '4px 4px 0 #000', white: '4px 4px 0 #fff' },
+      gridTemplateColumns: { auto: 'repeat(auto-fit, minmax(200px, 1fr))' },
+      animation: { 'fade-in': 'fadeIn 1s ease-out' },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0, transform: 'translateY(20px)' },
